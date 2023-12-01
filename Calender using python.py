e53c7d9a3999a7d16cd3896825f98d29f362510f
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-01 20:34:37 +0600

    Add files via upload

---
+++ Calender using python.py
@@ -0,0 +1,4 @@
+import calendar
+#if i change formatyear to formatmonth and mention the month in number like (2024,x)/(2024,y) then it will only show that mentioned month pf that year.
+
+print(calendar.TextCalendar(calendar.SUNDAY).formatyear(2024))
